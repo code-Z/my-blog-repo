0c69c2b295bbdd2cbe793264b5c88afffa20db5d
Mithilesh <s.mit7816@gmail.com>
2014-03-19 16:49:22 +0530

    pagination performed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,9 +5,9 @@ class ArticlesController < ApplicationController
 
 	def index
 		if params[:tag]
-		  @articles = Article.tagged_with(params[:tag]).paginate(page: params[:page], per_page: 3)
+		  @articles = Article.tagged_with(params[:tag]).paginate(page: params[:page], per_page: 10)
 		else
-		  @articles = Article.paginate(page: params[:page], per_page: 3)
+		  @articles = Article.paginate(page: params[:page], per_page: 10)
 		end
 	end
   

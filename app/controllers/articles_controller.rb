d1c1daa06e8283fd3dce75a3be017f3134ed6fde
Mithilesh <s.mit7816@gmail.com>
2014-03-19 11:29:08 +0530

    forms, flash, root features added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
+  
   def index
     @articles = Article.all
   end
@@ -6,4 +8,40 @@ class ArticlesController < ApplicationController
   def show
   	@article = Article.find(params[:id])
   end
+  
+  def new
+  	@article = Article.new
+  end
+  
+  def create
+ 		@article = Article.new(article_params)
+	  @article.save
+	  
+	  flash.notice = "Congratulations! you created a new article named '#{@article.title}' !"
+	  
+	  redirect_to article_path(@article)
+  end
+  
+  def destroy
+  	@article = Article.find(params[:id])
+  	@article.destroy
+  	
+  	flash.notice = "Successfully deleted the article named '#{@article.title}' !"
+  	
+  	redirect_to articles_path
+  end
+  
+  def edit
+  	@article = Article.find(params[:id])
+	end
+	
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+		
+		flash.notice = "You just updated the article '#{@article.title}' !"
+		
+		redirect_to article_path(@article)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -53,5 +53,8 @@ Blogger::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-	  resources :articles
+  
+  root to: 'articles#index'
+  
+	resources :articles
 end
